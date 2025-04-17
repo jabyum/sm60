3a1f8602147296b01782984a9d69a1bc7e22fa78
jab_yum <jab_yum@github.com>
2025-04-17 21:16:13 +0500

    second

---
+++ database/models.py
@@ -0,0 +1,48 @@
+from database import Base
+from sqlalchemy import (Column, Integer, String,
+                        ForeignKey, DateTime)
+from sqlalchemy.orm import relationship
+from datetime import datetime
+import pytz
+tashkent_timezone = pytz.timezone("Asia/Tashkent")
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, unique=True)
+    email = Column(String, unique=True)
+    phone_number = Column(String, unique=True)
+    city = Column(String, nullable=True)
+    birthday = Column(String, nullable=True)
+    password = Column(String)
+    reg_date = Column(DateTime, default=datetime.now(tashkent_timezone))
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    text = Column(String, nullable=True)
+    post_date = Column(DateTime, default=datetime.now(tashkent_timezone))
+    hashtags = Column(String, ForeignKey("hashtags.id"))
+    hashtags_fk = relationship("Hashtag", lazy="subquery")
+    users_fk = relationship(User, lazy="subquery")
+class Hashtag(Base):
+    __tablename__ = "hashtags"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    hashtag_name = Column(String, unique=True)
+    reg_date = Column(DateTime, default=datetime.now(tashkent_timezone))
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    text = Column(String)
+    created_at = Column(DateTime, default=datetime.now(tashkent_timezone))
+    author = Column(Integer, ForeignKey("users.id"))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    post_fk = relationship(Post, lazy="subquery")
+    user_fk = relationship(User, lazy="subquery")
+class Photo(Base):
+    __tablename__ = 'photos'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=True)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=True)
+    path = Column(String)
+    user_fk = relationship("User", lazy='subquery')
+    post_fk = relationship("Post", lazy='subquery')
