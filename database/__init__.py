85ed8f4e2d720c6eecc0c29200fbbd47c43f5bd7
jab_yum <jab_yum@github.com>
2025-04-26 13:46:48 +0500

    second

--- database/__init__.py
+++ database/__init__.py
@@ -1,9 +1,17 @@
-from sqlalchemy.engine import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import declarative_base, sessionmaker
 SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
+engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 localSession = sessionmaker(bind=engine)
 Base = declarative_base()
+
+engine = create_engine("sqlite:///your_database.db", echo=True)
+# включение каскадов
+@event.listens_for(engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON;")
+    cursor.close()
 
 def get_db():
     db = localSession()
--- database/models.py
+++ database/models.py
@@ -14,28 +14,28 @@ class User(Base):
     city = Column(String, nullable=True)
     birthday = Column(String, nullable=True)
     password = Column(String)
-    reg_date = Column(DateTime, default=datetime.now(tashkent_timezone))
+    reg_date = Column(DateTime, default=lambda: datetime.now(tashkent_timezone))
     post_fk = relationship("Post", back_populates="users_fk")
 class Post(Base):
     __tablename__ = 'posts'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"))
     text = Column(String, nullable=True)
-    post_date = Column(DateTime, default=datetime.now(tashkent_timezone))
-    hashtags = Column(String, ForeignKey("hashtags.id"))
+    post_date = Column(DateTime, default=lambda: datetime.now(tashkent_timezone))
+    hashtags = Column(Integer, ForeignKey("hashtags.id"))
     hashtags_fk = relationship("Hashtag", lazy="subquery")
     users_fk = relationship(User, lazy="subquery", back_populates="post_fk",
-                            cascade="all, delete", passive_deletes=True)
+                            cascade="all, delete-orphan", passive_deletes=True)
 class Hashtag(Base):
     __tablename__ = "hashtags"
     id = Column(Integer, primary_key=True, autoincrement=True)
     hashtag_name = Column(String, unique=True)
-    reg_date = Column(DateTime, default=datetime.now(tashkent_timezone))
+    reg_date = Column(DateTime, default=lambda: datetime.now(tashkent_timezone))
 class Comment(Base):
     __tablename__ = 'comments'
     id = Column(Integer, primary_key=True, autoincrement=True)
     text = Column(String)
-    created_at = Column(DateTime, default=datetime.now(tashkent_timezone))
+    created_at = Column(DateTime, default=lambda: datetime.now(tashkent_timezone))
     author = Column(Integer, ForeignKey("users.id"))
     post_id = Column(Integer, ForeignKey('posts.id'))
     post_fk = relationship(Post, lazy="subquery")
@@ -44,7 +44,7 @@ class Photo(Base):
     __tablename__ = 'photos'
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=True)
-    post_id = Column(Integer, ForeignKey('post.id'), nullable=True)
+    post_id = Column(Integer, ForeignKey('posts.id'), nullable=True)
     path = Column(String)
     user_fk = relationship("User", lazy='subquery')
     post_fk = relationship("Post", lazy='subquery')
